8d636167f1b9604033db23b9f1625b9a3b3a0c10
Gabriel Medina <gmedina.santos@gmail.com>
2024-03-18 14:18:11 -0400

    feat: player repository

---
+++ src/main/java/com/api/pokerclub/repositories/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.api.pokerclub.repositories;
+
+import com.api.pokerclub.models.PlayerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<PlayerModel, UUID> {
+}
