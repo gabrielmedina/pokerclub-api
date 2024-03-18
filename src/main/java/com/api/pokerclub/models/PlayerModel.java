2e0bf6ed59a89b6211cafa1a36b9a7e92d10a8a7
Gabriel Medina <gmedina.santos@gmail.com>
2024-03-18 14:14:20 -0400

    feat: player model

---
+++ src/main/java/com/api/pokerclub/models/PlayerModel.java
@@ -0,0 +1,36 @@
+package com.api.pokerclub.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+public class PlayerModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
