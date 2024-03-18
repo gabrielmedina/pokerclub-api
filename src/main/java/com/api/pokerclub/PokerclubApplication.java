42adcf803ca11920c51a671a4eccc22a9c59673a
Gabriel Medina <gmedina.santos@gmail.com>
2024-03-18 14:10:36 -0400

    init project

---
+++ src/main/java/com/api/pokerclub/PokerclubApplication.java
@@ -0,0 +1,11 @@
+package com.api.pokerclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerclubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PokerclubApplication.class, args);
+	}
+}
---
+++ src/test/java/com/api/pokerclub/PokerclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pokerclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
