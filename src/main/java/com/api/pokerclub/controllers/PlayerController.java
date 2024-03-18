22c93d4e13dc2076014c1a07941eeef031532e3f
Gabriel Medina <gmedina.santos@gmail.com>
2024-03-18 14:52:50 -0400

    feat: get one and all players

--- src/main/java/com/api/pokerclub/controllers/PlayerController.java
+++ src/main/java/com/api/pokerclub/controllers/PlayerController.java
@@ -6,9 +6,16 @@ import com.api.pokerclub.services.PlayerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -20,11 +27,27 @@ public class PlayerController {
     @PostMapping
     public ResponseEntity<Object> savePlayer(@RequestBody @Valid PlayerDto playerDto) {
         if (playerService.existsByName(playerDto.getName())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: player name in use");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: player name in use.");
         }
 
         var playerModel = new PlayerModel();
         BeanUtils.copyProperties(playerDto, playerModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(playerService.save(playerModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PlayerModel>> getAllPlayers(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(playerService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOnePlayer(@PathVariable(value = "id") UUID id) {
+        Optional<PlayerModel> playerModelOptional = playerService.findById(id);
+
+        if (!playerModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player not found.");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(playerModelOptional.get());
     }
 }
--- src/main/java/com/api/pokerclub/services/PlayerService.java
+++ src/main/java/com/api/pokerclub/services/PlayerService.java
@@ -4,7 +4,12 @@ import com.api.pokerclub.models.PlayerModel;
 import com.api.pokerclub.repositories.PlayerRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PlayerService {
@@ -14,6 +19,19 @@ public class PlayerService {
     @Transactional
     public PlayerModel save(PlayerModel playerModel) {
         return playerRepository.save(playerModel);
+    }
+
+    public Page<PlayerModel> findAll(Pageable pageable) {
+        return playerRepository.findAll(pageable);
+    }
+
+    public Optional<PlayerModel> findById(UUID id) {
+        return playerRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(PlayerModel playerModel) {
+        playerRepository.delete(playerModel);
     }
 
     public boolean existsByName(String name) {
